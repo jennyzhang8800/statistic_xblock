dc5e9fa94ed2aa36b752c2aaa449bc48bb6e025a
JennyZhang <jennyzhang8800@163.com>
2016-12-19 12:01:01 +0800

    Delete statistic_new_commit.sh

--- statistic_xblock_document/Script/statistic_new_commit.sh
+++
@@ -1,5 +0,0 @@
-message=$1
-cd /var/www/zyni/answer/
-echo ${message}
-git log --since=${message} --pretty=format:"%ad,%s"
-git log --since=${message} --pretty=format:"%ad,%s" >/var/www/zyni/gitlab_answer_gitlog.log
